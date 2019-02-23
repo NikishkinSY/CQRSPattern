31763a4b5c89eac06cd26cadb55b2eb7db3c7de7
Sergey Nikishkin <sergey@gohopscotch.com>
2019-02-23 14:32:30 +0300

    add main program code

--- CQRSPattern/Program.cs
+++ CQRSPattern/Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿using DAL;
+using DAL.CQRS;
+using DAL.CQRS.Commands;
+using DAL.CQRS.Queries;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Attribute = DAL.DAO.Attribute;
 
 namespace CQRSPattern
 {
@@ -6,7 +15,28 @@ namespace CQRSPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // init dependency injection service
+            var provider = DependencyInjection.Init("Data Source=AttributesDb.db");
+
+            // open db connection
+            var context = provider.GetService<AttributeContext>();
+            context.Database.OpenConnection();
+
+            // create db and fill with data
+            DbValidator.CheckDb(context);
+
+            // update command
+            var updateAttributeNameCommandHandler = provider.GetService<ICommandHandler<UpdateAttributeNameCommand>>();
+            updateAttributeNameCommandHandler.Handle(new UpdateAttributeNameCommand("newAttributeName1", 1));
+
+            // get query
+            var getAttributesQueryHandler = provider.GetService<IQueryHandler<GetAttributesQuery, IEnumerable<Attribute>>>();
+            var attributes = getAttributesQueryHandler.Handle(new GetAttributesQuery()).ToList();
+
+            // close connection
+            context.Database.CloseConnection();
+
+            Console.ReadKey();
         }
     }
 }
