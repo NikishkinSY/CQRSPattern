c1db2f261f822fd89a7f9f0bc75baf7d579ce58d
Sergey Nikishkin <sergey@gohopscotch.com>
2019-02-23 13:44:39 +0300

    add db context

---
+++ DAL/AttributeContext.cs
@@ -0,0 +1,16 @@
+﻿using DAL.DAO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DAL
+{
+    public class AttributeContext: DbContext
+    {
+        public AttributeContext(DbContextOptions<AttributeContext> options)
+            : base(options)
+        {
+        }
+        
+        public DbSet<Attribute> Attributes { get; set; }
+    }
+}
---
+++ DAL/DAO/Attribute.cs
@@ -0,0 +1,18 @@
+﻿namespace DAL.DAO
+{
+    public class Attribute
+    {
+        public Attribute(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        public Attribute()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
