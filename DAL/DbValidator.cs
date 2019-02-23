64da5f07e14e96fc31f11c1f78d57cd1e9111199
Sergey Nikishkin <sergey@gohopscotch.com>
2019-02-23 14:31:00 +0300

    add db validator class

---
+++ DAL/DbValidator.cs
@@ -0,0 +1,20 @@
+﻿using DAL.DAO;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace DAL
+{
+    public static class DbValidator
+    {
+        public static void CheckDb(AttributeContext context)
+        {
+            context.Database.EnsureCreated();
+            if (!context.Attributes.Any())
+            {
+                context.Add(new Attribute(1, "Attribute1"));
+                context.Add(new Attribute(2, "Attribute2"));
+                context.Add(new Attribute(3, "Attribute3"));
+                context.SaveChanges();
+            }
+        }
+    }
+}
